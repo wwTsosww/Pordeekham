69ed5ff6a17191844d43d63421bfe68fac3cee3e
junkuku <junkeddid@gmail.com>
2025-07-15 11:57:57 +0700

    Update

--- src/components/BMR.jsx
+++ src/components/BMR.jsx
@@ -1,75 +1,61 @@
 import { useContext } from "react";
-import { UserDataContext } from "../context/UserDataContext"; // อย่าลืม path
+import { UserDataContext } from "../context/UserDataContext";
 import "../styles/BMR.css";
 
 function BMR({ onClose }) {
   const { userData } = useContext(UserDataContext);
   const { bmi, bmr, tdee } = userData || {};
+
   return (
-    <div className="credits-main-box">
-      <div className="credits-second-box">
-        <button className="credits-exit-button" onClick={onClose}>
+    <div className="BMR-main-box">
+      <div className="BMR-second-box">
+        <button className="BMR-exit-button" onClick={onClose}>
           X
         </button>
-        <div className="credits-title-box">
-          <h1 className="credits-title">BMR</h1>
+
+        <div className="BMR-title-box">
+          <h1 className="BMR-title-text">BMR</h1>
         </div>
-        <div>
-          <div className="BMR-dcotext">
-            <h>วันนี้ขอเสนอ. . . .</h>
-          </div>
-          <div className="hyper-box">
+
+        <div className="BMR-bubble">
+          <h>วันนี้ขอเสนอ. . . .</h>
+        </div>
+
+        <div className="BMR-third-box">
+          <div className="BMR-third-box-1">
             <img
-              className="BMR-doc"
+              className="BMR-img"
               src={`${process.env.PUBLIC_URL}/images/food_data/logo/nurse.PNG`}
-              alt="doc"
-            ></img>
+              alt="BMR-img"
+            />
+          </div>
+
+          <div className="BMR-info-box">
             <div className="BMR-box">
-              <div className="BMR-box-details">
-                <h>BMI:</h>
-                <h></h>
-                {bmi.toFixed(2)}
-              </div>
-              <div className="BMR-box-details">
-                <h>BMR:</h>
-                <h></h>
-                {bmr.toFixed(2)}
-              </div>
-              <div className="BMR-box-details">
-                <h>TDEE:</h>
-                <h></h>
-                {tdee.toFixed(2)}
-              </div>
+              <div className="BMR-box-details">BMI: {bmi.toFixed(2)}</div>
+              <div className="BMR-box-details">BMR: {bmr.toFixed(2)}</div>
+              <div className="BMR-box-details">TDEE: {tdee.toFixed(2)}</div>
+            </div>
+            <div className="BMR-end-text">
+              <h>
+                หากต้องการลดน้ำหนัก แนะนำให้ลดปริมาณพลังงานให้น้อยกว่าที่ร่างกายใช้วันละ
+              </h>
+              <h>
+                ประมาณ 500 แคลอรี่ แต่เมื่อลบออก 500 แคลอรี่แล้ว ห้ามน้อยกว่าค่า BMR
+                เพราะจะทำให้
+              </h>
+              <h>
+                ร่างกายได้รับพลังงานน้อยเกินไป
+                เมื่อร่างกายเรียนรู้ว่าได้รับพลังงานน้อยมาก ก็จะไม่ยอมใช้
+              </h>
+              <h>
+                พลังงานตามปกติ อาจส่งผลต่อระบบเผาผลาญ
+              </h>
             </div>
           </div>
         </div>
-
-        <div className="end-text">
-          <h>
-            หากต้องการลดน้ำหนัก
-            แนะนำให้ลดปริมาณพลังงานให้น้อยกว่าที่ร่างกายใช้วันละ
-          </h>
-          <h>
-            ประมาณ 500 แคลอรี่ แต่เมื่อลบออก 500 แคลอรี่แล้ว ห้ามน้อยกว่าค่า BMR
-            เพราะจะทำให้
-          </h>
-          <h>
-            ร่างกายได้รับพลังงานน้อยเกินไป
-            เมื่อร่างกายเรียนรู้ว่าได้รับพลังงานน้อยมาก ก็จะไม่ยอมใช้
-          </h>
-          <h>พลังงานตามปกติ อาจส่งผลต่อระบบเผาผลาญ</h>
-        </div>
-        <div
-          className="credits-title-box"
-          onClick={() =>
-            window.open(
-              "https://nutrilite.co.th/th/article/what-is-bmr#:~:text=...",
-              "_blank"
-            )
-          }
-          style={{ cursor: "pointer" }}
-        >
-          <h className="credits-title-1">เพิ่มเติม</h>
+        <div className="BMR-more-button" onClick={() => window.open("https://nutrilite.co.th/th/article/what-is-bmr#:~:text=...", "_blank")}>
+          <h>เพิ่มเติม</h>
         </div>
       </div>
     </div>
